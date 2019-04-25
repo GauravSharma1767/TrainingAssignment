ff712d83b5fc152978326aa5b859e653bbed4136
Gaurav Sharma <gaurav.sharma@intimetec.com>
2019-04-25 22:01:37 +0530

    SwitchCaseAssignment

---
+++ SwitchMain.java
@@ -0,0 +1,27 @@
+
+public class SwitchMain {
+
+	public static void main(String[] args)
+	{
+		int day = 1;
+		switch(day)
+		{
+			case 1:
+			{
+				System.out.println("Monday");break;
+				
+			}
+			case 2:
+			{
+				System.out.println("Tuesday");
+				
+			}
+			default:
+			{
+				System.out.println("incorrect input");
+				
+			}
+			
+		}
+	}
+}
